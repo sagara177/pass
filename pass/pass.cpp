26dc601b97466373301ba39fcd75d5aafe1c4346
Yukinori Sagara <sagara177@gmail.com>
2012-10-21 11:46:21 +0900

    add capitalizing feature.

--- pass/pass.cpp
+++ pass/pass.cpp
@@ -12,8 +12,8 @@
 
 #include <boost/random.hpp>
 #include <boost/foreach.hpp>
-
-#include "Capitalize.h";
+#include <boost/algorithm/string.hpp>
+#include <boost/range.hpp>
 
 const std::string CHARS =
 		"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -24,16 +24,25 @@ typedef boost::mt19937 Engine;
 typedef boost::uniform_smallint<> Distribution;
 typedef boost::variate_generator<Engine, Distribution> Generator;
 
-std::string password(Generator& rand) {
+std::string password(Generator& rand)
+{
 	std::string pass = "";
 	for (int i = 0; i < LENGTH; ++i)
 		pass += CHARS[rand()];
 	return pass;
 }
 
-int main() {
+void capitalize(std::string& str)
+{
+	boost::to_lower(str);
+	boost::iterator_range<std::string::iterator> r(
+			boost::make_iterator_range(str.begin(), str.begin() + 1));
+	boost::to_upper(r);
+}
+
+int main()
+{
 	std::vector<std::string> passwords;
-	std::vector<std::string> capitalized;
 
 	Engine gen(static_cast<unsigned long>(time(0)));
 	Distribution dst(0, CHARS.length() - 1);
@@ -42,11 +51,11 @@ int main() {
 	for (int i = 0; i < NUM; ++i)
 		passwords.push_back(password(rand));
 
-	std::transform(passwords.begin(), passwords.end(), passwords.begin(),
-			Capitalize());
+	BOOST_FOREACH(std::string& pass, passwords)
+		capitalize(pass);
 
-	BOOST_FOREACH(std::string& s, passwords)
-		std::cout << s << std::endl;
+	BOOST_FOREACH(std::string& pass, passwords)
+		std::cout << pass << std::endl;
 
 	return EXIT_SUCCESS;
 }
